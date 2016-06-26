94d34f51f6c36e1dfd6793713a1657e6f1a33d6b
Alex Tenche <alex.tenche@gmail.com>
2016-06-26 22:25:01 +0300

    finished tutorial, still to fix jquery conflict

--- js/album.js
+++ js/album.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
   $('.gallery_thumbnails a').click(function(event){
     event.preventDefault();
 
-    $('.gallery_thumbnails a').removeClass('selected');
-    $('.gallery_thumbnails a').children().css('opacity', '1');
-    $(this).addClass('selected');
-    $(this).children().css('opacity', '.4');
+    // $('.gallery_thumbnails a').removeClass('selected');
+    // $('.gallery_thumbnails a').children().css('opacity', '1');
+    // $(this).addClass('selected');
+    // $(this).children().css('opacity', '.4');
 
     var photo_caption = $(this).attr('title');
     var photo_fullsize = $(this).attr('href');
@@ -24,11 +24,35 @@ $(document).ready(function(){
         $('.gallery_caption').slideDown(500);
 
         setFancyboxLinks();
+        updateThumbnails()
       });
 
     });
   });
+
+  // initialize gallery on load
+  var first_photo_caption = $('.gallery_thumbnails a').first().attr('title');
+  var first_photo_fullsize = $('.gallery_thumbnails a').first().attr('href');
+  var first_photo_preview = first_photo_fullsize.replace('_fullsize', '_preview');
+
+  $('.gallery_caption').slideUp(500);
+  $('.gallery_preview').fadeOut(500, function(){
+
+    $('.gallery_preload_area').html('<img src="' + first_photo_preview + '"/>');
+    $('.gallery_preload_area img').imgpreload(function(){
+      $('.gallery_preview').html('<a class="overlaylink" href="' + first_photo_fullsize + '" title="' + first_photo_caption + '" style="background-image:url(' + first_photo_preview + ');"><img src="'+ first_photo_preview+'"/></a>');
+      $('.gallery_caption').html('<p><a class="overlaylink" href="' + first_photo_fullsize + '" title="' + first_photo_caption + '">view larger</a></p><p>' + first_photo_caption + '</p>');
+      $('.gallery_preview').fadeIn(500);
+      $('.gallery_caption').slideDown(500);
+
+      setFancyboxLinks();
+      updateThumbnails()
+    });
+
+  });
+
 });
+
 
 function setFancyboxLinks(){
   $('a.overlaylink').fancybox({
@@ -38,5 +62,18 @@ function setFancyboxLinks(){
     'transitionOut': 'elastic',
     'transitionIn': 'elastic',
     'autoScale': true
+  });
+}
+
+
+function updateThumbnails(){
+  $('.gallery_thumbnails a').each(function(){
+    if($('.gallery_preview a').attr('href') ==$(this).attr('href')){
+      $(this).addClass('selected');
+      $(this).children().fadeTo(250, .4);
+    } else {
+      $(this).removeClass('selected');
+      $(this).children().css('opacity', '1');
+    }
   });
 }
